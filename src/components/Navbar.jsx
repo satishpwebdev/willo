4d5b3579c6c26b72433856363a94e9c58ad78801
Satish <satishpwebdev@gmail.com>
2024-09-09 00:08:28 +0530

    intial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <section>
+      <div className='flex items-center justify-center w-full'>
+        <h2>Navbar</h2>
+      </div>
+    </section>
+  )
+}
+
+export default Navbar
